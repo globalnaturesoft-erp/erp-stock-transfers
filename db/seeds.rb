28732653b84bfe7c0d2aeb5b3851b7e6dea1eda7
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-07-26 23:18:45 -0700

    update seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,58 +1,51 @@
-puts "---------- Stock Transfers Engine ------------"
 user = Erp::User.first
+warehouses = ["ADV", "TRAINING", "TN"]
 
-puts "Create sample contact"
-Erp::Contacts::Contact.where(name: "Ortho-K Viet Nam").destroy_all
+# Contacts
+Erp::Contacts::Contact.where(name: "Ortho-K Vietnam").destroy_all
 owner = Erp::Contacts::Contact.create(
   contact_type: Erp::Contacts::Contact::TYPE_COMPANY,
-  name: "Ortho-K Viet Nam",
+  name: "Ortho-K Vietnam",
   code: "#OTK-01C",
   address: "535 An Duong Vuong, Ward 8, Dist.5, HCMC",
   creator_id: user.id
 )
 puts owner.errors.to_json if !owner.errors.empty?
 
-puts "Create sample warehouse"
-Erp::Warehouses::Warehouse.where(name: "Warehouse 1st").destroy_all
-warehouse1 = Erp::Warehouses::Warehouse.create(
-  name: "Warehouse 1st",
-  short_name: "WH-1ST",
-  creator_id: user.id,
-  contact_id: owner.id
-)
-puts warehouse1.errors.to_json if !warehouse1.errors.empty?
+# Warehouses
+Erp::Warehouses::Warehouse.all.destroy_all
+warehouses.each do |name|
+  wh = Erp::Warehouses::Warehouse.create(
+    name: name,
+    short_name: name,
+    creator_id: user.id,
+    contact_id: owner.id
+  )
+end
 
-Erp::Warehouses::Warehouse.where(name: "Warehouse 2nd").destroy_all
-warehouse2 = Erp::Warehouses::Warehouse.create(
-  name: "Warehouse 2nd",
-  short_name: "WH-2ND",
-  creator_id: user.id,
-  contact_id: owner.id
-)
-puts warehouse2.errors.to_json if !warehouse2.errors.empty?
-
-puts "Create sample transfer"
-Erp::StockTransfers::Transfer.where(code: "ST001").destroy_all
-transfer = Erp::StockTransfers::Transfer.create(
-  code: "ST001",
-  received_at: Time.now,
-  source_warehouse_id: warehouse1.id,
-  destination_warehouse_id: warehouse2.id,
-  creator_id: user.id
-)
-puts transfer.errors.to_json if !transfer.errors.empty?
-
-puts "Create sample transfer detail"
-transfer_detail_1 = Erp::StockTransfers::TransferDetail.create(
-  product_id: Erp::Products::Product.first.id,
-  transfer_id: transfer.id,
-  quantity: 2
-)
-puts transfer_detail_1.errors.to_json if !transfer_detail_1.errors.empty?
-
-transfer_detail_2 = Erp::StockTransfers::TransferDetail.create(
-  product_id: Erp::Products::Product.last.id,
-  transfer_id: transfer.id,
-  quantity: 1
-)
-puts transfer_detail_2.errors.to_json if !transfer_detail_2.errors.empty?
+# Stock Transfers
+Erp::StockTransfers::Transfer.all.destroy_all
+count = 0
+count_b = 0
+(1..50).each do |num|
+  ws = Erp::Warehouses::Warehouse.where(id: Erp::Warehouses::Warehouse.pluck(:id).sample(2))
+  transfer = Erp::StockTransfers::Transfer.create(
+    code: 'ST'+ num.to_s.rjust(3, '0'),
+    received_at: Time.now,
+    source_warehouse_id: ws.first.id,
+    destination_warehouse_id: ws.second.id,
+    creator_id: user.id
+  )
+  count_b += 1
+  puts "BIG: " + count_b.to_s
+  Erp::Products::Product.where(id: Erp::Products::Product.pluck(:id).sample(rand(90..200))).each do |product|
+    dd = Erp::StockTransfers::TransferDetail.create(
+      product_id: product.id,
+      transfer_id: transfer.id,
+      quantity: rand(1..3)
+    )
+    count += 1
+    puts "###########################BIG: " + count_b.to_s + " / " + count.to_s
+  end
+  
+end
