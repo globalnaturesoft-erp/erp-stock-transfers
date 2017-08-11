931327100856242c4dbed765dd464ff0dd84f084
Hung Nguyen <hungnt@hoangkhang.com.vn>
2017-08-11 02:03:43 -0700

    update seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,6 @@ status = [Erp::StockTransfers::Transfer::STATUS_DRAFT,
 
 # Stock Transfers
 Erp::StockTransfers::Transfer.all.destroy_all
-count = 0
-count_b = 0
 (1..50).each do |num|
   ws = Erp::Warehouses::Warehouse.where(id: Erp::Warehouses::Warehouse.pluck(:id).sample(2))
   transfer = Erp::StockTransfers::Transfer.create(
@@ -18,16 +16,11 @@ count_b = 0
     status: status[rand(status.count)],
     creator_id: user.id
   )
-  count_b += 1
-  puts "BIG: " + count_b.to_s
   Erp::Products::Product.where(id: Erp::Products::Product.pluck(:id).sample(rand(90..200))).each do |product|
     dd = Erp::StockTransfers::TransferDetail.create(
       product_id: product.id,
       transfer_id: transfer.id,
       quantity: rand(1..3)
     )
-    count += 1
-    puts "###########################BIG: " + count_b.to_s + " / " + count.to_s
   end
-  
 end
