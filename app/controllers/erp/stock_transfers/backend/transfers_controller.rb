849847791f058481dd9962518c63ded9d9a47c6b
Luan Pham <luanpm88@gmail.com>
2017-08-16 23:27:45 -0700

    update seed

--- app/controllers/erp/stock_transfers/backend/transfers_controller.rb
+++ app/controllers/erp/stock_transfers/backend/transfers_controller.rb
@@ -5,56 +5,89 @@ module Erp
         before_action :set_transfer, only: [:show, :edit, :update, :destroy,
                                             :set_draft, :set_activate, :set_delivery, :set_remove]
         before_action :set_transfers, only: [:set_activate_all, :set_delivery_all, :set_remove_all]
-    
+
         # GET /transfers
         def index
         end
-        
+
         # POST /transfers/list
         def list
           @transfers = Transfer.search(params).paginate(:page => params[:page], :per_page => 10)
-          
+
           if params.to_unsafe_hash[:global_filter].present? and params.to_unsafe_hash[:global_filter][:transfer_from_date].present?
             @transfers = @transfers.where('received_at >= ?', params.to_unsafe_hash[:global_filter][:transfer_from_date].to_date.beginning_of_day)
           end
 
-          if params.to_unsafe_hash[:global_filter].present? and params.to_unsafe_hash[:global_filter][:transfer_to_date].present? 
+          if params.to_unsafe_hash[:global_filter].present? and params.to_unsafe_hash[:global_filter][:transfer_to_date].present?
             @transfers = @transfers.where('received_at <= ?', params.to_unsafe_hash[:global_filter][:transfer_to_date].to_date.end_of_day)
           end
-          
+
           render layout: nil
         end
-        
+
         # GET /transfer details
         def transfer_details
           @transfer = Transfer.find(params[:id])
-          
+
           render layout: nil
         end
-    
+
         # GET /transfers/1
         def show
         end
-    
+
         # GET /transfers/new
         def new
           @transfer = Transfer.new
           @transfer.received_at = Time.now
-          
+
+          if params[:from_warehouse].present?
+            @from_warehouse = params[:from_warehouse].present? ? Erp::Warehouses::Warehouse.find(params[:from_warehouse]) : nil
+            @to_warehouse = params[:to_warehouse].present? ? Erp::Warehouses::Warehouse.find(params[:to_warehouse]) : nil
+            @transfer_quantity = params[:transfer_quantity]
+
+            @condition = params[:condition]
+            @condition_value = params[:condition_value]
+
+            ids = Erp::Products::Product.pluck(:id).sample(rand(90..250))
+            @products = Erp::Products::Product.where(id: ids).order(:code)
+
+            @transfer.source_warehouse_id = @from_warehouse.id
+            @transfer.destination_warehouse_id = @to_warehouse.id
+
+            @products.each do |product|
+              if @condition == 'from_redundant'
+                from = rand(@condition_value.to_i..(@condition_value.to_i+4))
+                to = rand(0..2)
+                transfer = @transfer_quantity
+              else
+                to = rand(0..@condition_value.to_i)
+                from = rand(1..6)
+                transfer = from > @transfer_quantity.to_i ? @transfer_quantity.to_i : from
+              end
+
+              @transfer.transfer_details.build(
+                quantity: transfer,
+                product_id: product.id
+              )
+            end
+          end
+
           if request.xhr?
             render '_form', layout: nil, locals: {transfer: @transfer}
           end
         end
-    
+
         # GET /transfers/1/edit
         def edit
         end
-    
+
         # POST /transfers
         def create
           @transfer = Transfer.new(transfer_params)
           @transfer.creator = current_user
-    
+          @transfer.status = Transfer::STATUS_DRAFT
+
           if @transfer.save
             if request.xhr?
               render json: {
@@ -73,7 +106,7 @@ module Erp
             end
           end
         end
-    
+
         # PATCH/PUT /transfers/1
         def update
           if @transfer.update(transfer_params)
@@ -91,11 +124,11 @@ module Erp
             render :edit
           end
         end
-    
+
         # DELETE /transfers/1
         def destroy
           @transfer.destroy
-          
+
           respond_to do |format|
             format.html { redirect_to erp_stock_transfers.backend_transfers_path, notice: t('.success') }
             format.json {
@@ -106,11 +139,11 @@ module Erp
             }
           end
         end
-        
+
         # Activate /transfers/status?id=1
         def set_activate
           @transfer.set_activate
-          
+
           respond_to do |format|
           format.json {
             render json: {
@@ -120,11 +153,11 @@ module Erp
           }
           end
         end
-        
+
         # Delivery /transfers/status?id=1
         def set_delivery
           @transfer.set_delivery
-          
+
           respond_to do |format|
           format.json {
             render json: {
@@ -134,11 +167,11 @@ module Erp
           }
           end
         end
-        
+
         # Remove /transfers/status?id=1
         def set_remove
           @transfer.set_remove
-          
+
           respond_to do |format|
           format.json {
             render json: {
@@ -148,11 +181,11 @@ module Erp
           }
           end
         end
-        
+
         # ACTIVATE ALL /transfers/status?ids=1,2,3
-        def set_activate_all         
+        def set_activate_all
           @transfers.set_activate_all
-          
+
           respond_to do |format|
             format.json {
               render json: {
@@ -160,13 +193,13 @@ module Erp
                 'type': 'success'
               }
             }
-          end          
+          end
         end
-        
+
         # DELIVERY ALL /transfers/status?ids=1,2,3
-        def set_delivery_all         
+        def set_delivery_all
           @transfers.set_delivery_all
-          
+
           respond_to do |format|
             format.json {
               render json: {
@@ -174,13 +207,13 @@ module Erp
                 'type': 'success'
               }
             }
-          end          
+          end
         end
-        
+
         # REMOVE ALL /transfers/status?ids=1,2,3
-        def set_remove_all         
+        def set_remove_all
           @transfers.set_remove_all
-          
+
           respond_to do |format|
             format.json {
               render json: {
@@ -188,19 +221,19 @@ module Erp
                 'type': 'success'
               }
             }
-          end          
+          end
         end
-    
+
         private
           # Use callbacks to share common setup or constraints between actions.
           def set_transfer
             @transfer = Transfer.find(params[:id])
           end
-          
+
           def set_transfers
             @transfers = Transfer.where(id: params[:ids])
           end
-    
+
           # Only allow a trusted parameter "white list" through.
           def transfer_params
             params.fetch(:transfer, {}).permit(:code, :received_at, :source_warehouse_id, :destination_warehouse_id, :note,
