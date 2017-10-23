654348c957543565a8edb76fbd525d39f5cf2770
Luan Pham <luanpm88@gmail.com>
2017-10-22 20:42:16 -0700

    fix saveing price

--- app/models/erp/stock_transfers/transfer_detail.rb
+++ app/models/erp/stock_transfers/transfer_detail.rb
@@ -3,38 +3,42 @@ module Erp::StockTransfers
     belongs_to :product, class_name: 'Erp::Products::Product'
     belongs_to :transfer, inverse_of: :transfer_details
     belongs_to :state, class_name: 'Erp::Products::State'
-    
+
     after_save :update_transfer_cache_products_count
-    
+
+    def quantity=(number)
+      self[:quantity] = number.to_s.gsub(/\,/, '')
+    end
+
     # update order cache products count
     def update_transfer_cache_products_count
 			if transfer.present?
 				transfer.update_cache_products_count
 			end
 		end
-    
+
     def product_code
       product.nil? ? '' : product.code
     end
-    
+
     def product_name
       product.nil? ? '' : product.name
     end
-    
+
     def state_name
 			state.nil? ? '' : state.name
 		end
-    
+
     # @todo: hard code for stock_source and stock_destination
     # Available stock at source
     def stock_source
 			return 50
 		end
-    
+
     # Available stock at destination
     def stock_destination
 			return 50
 		end
-    
+
   end
 end
